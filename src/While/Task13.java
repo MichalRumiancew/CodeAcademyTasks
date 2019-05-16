bf34afccf1ea286348e5658647d3eadfe09775f9
Michal <michalr11@gmail.com>
2019-05-16 12:24:13 +0200

    commit

---
+++ src/While/Task13.java
@@ -0,0 +1,12 @@
+package While;
+
+import java.util.Scanner;
+
+public class Task13 {
+    public static void main(String[] args) {
+
+        for (int i = 0; i <=19 ; i++) {
+            System.out.print("*");
+        }
+    }
+}

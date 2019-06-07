46e7f8f2013855edaf42cd7ddb46afc365758ae7
Michal <michalr11@gmail.com>
2019-06-07 22:07:09 +0200

    Commit task 14 methods

---
+++ src/methods/Task14.java
@@ -0,0 +1,21 @@
+package methods;
+
+public class Task14 {
+
+    public static void main (String[] args) {
+
+
+        int max = 2;
+        for (int i = 2; i < 10 * 1000; i++) {
+            if (Task11.calculateNumberOfDividers(i) > Task11.calculateNumberOfDividers(max))
+
+                max = i;
+
+
+        }
+        System.out.println("Liczba, która ma największą liczbę dzielników to  " + max + " liczba jej dzielników to: "
+                + Task11.calculateNumberOfDividers(max));
+
+
+    }
+}

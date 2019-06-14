ec49ca9d7724e7d5480ad63e088eb2e56e6b94d1
Michal <michalr11@gmail.com>
2019-06-14 20:58:15 +0200

    Commit Method Task15 is Palindrome

---
+++ src/arrays/Task14ToLowerCase.java
@@ -0,0 +1,12 @@
+package arrays;
+
+public class Task14ToLowerCase {
+    String[] toLowerCases(String[] array) {
+        String[] newArray = new String[array. length ];
+        for ( int i = 0 ; i < array. length ; i++) {
+            newArray[i] = array[i].toLowerCase();
+        }
+        return newArray;
+    }
+}
+

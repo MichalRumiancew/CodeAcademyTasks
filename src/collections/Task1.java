77d2027c0e9fb1b3d1201f9c33b1577b54138275
Michal <michalr11@gmail.com>
2019-06-18 13:11:23 +0200

    Commit Method Task15 is Palindrome

---
+++ src/collections/Task1.java
@@ -0,0 +1,15 @@
+package collections;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Task1 {
+    static List<Integer>
+    getDivadedByThtee(List<Integer> numbers) {
+        List<Integer> elements = new ArrayList<>();
+        for (Integer value : numbers) {
+            if (value % 3 == 0) ;
+        }
+        return elements;
+    }
+}

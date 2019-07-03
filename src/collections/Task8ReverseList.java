1354294bf6c52f8d2d67dfd3bbf85400e9e032dd
Michal <michalr11@gmail.com>
2019-07-03 13:44:14 +0200

    Colors task 8

--- src/collections/Task8ReverseList.java
+++ src/collections/Task8ReverseList.java
@@ -1,4 +1,17 @@
 package collections;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class Task8ReverseList {
+    public static List<Integer> reverse(List<Integer> list) {
+        Collections.reverse(list);
+        return list;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(reverse(Arrays.asList(5, 1, 99)));
+    }
+
 }
